ab912b94dd6049563408bf44bf8e74c50eec2d75
nhutnoob <88940070+noobnhut@users.noreply.github.com>
2023-03-05 23:09:55 +0700

    DĐ

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,16 +6,9 @@
  <router-view name="about"/>
  <router-view name="blog"/>
  <router-view></router-view>
- <footers/>
+
 </template>
 
 <script>
-import footers from './components/footer.vue';
-export default{
-    components:
-{
-    footers
-}
-}
 
 </script>
--- client/src/views/client.vue
+++ client/src/views/client.vue
@@ -1,8 +1,16 @@
 <template>
      
     <router-view/>
-
+    <footers/>
 </template>
 <script>
 import "../assets/client.css";
+import footers from '../components/footer.vue';
+export default{
+    components:
+{
+    footers
+}
+}
+
 </script>
