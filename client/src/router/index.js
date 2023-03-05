21bf1444b6f64be5daadd277e4f7058964299253
nhutnoob <88940070+noobnhut@users.noreply.github.com>
2023-03-05 09:52:17 +0700

    up

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,7 +2,6 @@ import admin from '../views/admin.vue';
 import client from '../views/client.vue';
 import {createRouter,createWebHistory, useRoute} from 'vue-router';
 const router = createRouter({
-  history: createWebHistory(),
   routes: [ 
     {
         path: '/',
