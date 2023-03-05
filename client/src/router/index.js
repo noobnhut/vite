18a82a27d3a1a61f6243cefbd7e49ff3f1c983b1
nhutnoob <88940070+noobnhut@users.noreply.github.com>
2023-03-05 10:07:17 +0700

    sss

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [ 
      {
-        path: '/client',
+        path: '/',
         components: {
           default: client,        
         }, 
