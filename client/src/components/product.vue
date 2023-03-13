44c94295a88f79156612b182785558dfcd56f78a
nhutnoob <88940070+noobnhut@users.noreply.github.com>
2023-03-13 22:51:57 +0700

    a

--- client/src/components/product.vue
+++ client/src/components/product.vue
@@ -26,7 +26,7 @@
                 </small>
               </p>
               <!--@click="addCart(product.id)"-->
-              <button  @click="addCart(product.id)" class="btn btn-primary mt-3 position-relative "   >Mua hàng</button>
+              <button  @click="addCart(product.id)" class="btn btn-primary mt-3 position-relative " data-bs-toggle="modal" data-bs-target="#exampleModal"  >Mua hàng</button>
             </div>
 
           </div>
@@ -88,7 +88,6 @@ export default {
   },
   mounted() {
     this.getproducts(); 
-    this.getcart();
   },
   methods:
   {
